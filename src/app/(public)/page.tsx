c544febf18da888f69940ac5b8b52d5e3a4e8412
Baki Abdullah <shatil605@gmail.com>
2025-10-01 19:13:10 +0600

    Basic laylout setup done

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,12 @@
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2 className="text-center my-5 text-5xl text-slate-900 font-extrabold">
+        This is Home Page
+      </h2>
+    </div>
+  );
+};
+
+export default HomePage;
